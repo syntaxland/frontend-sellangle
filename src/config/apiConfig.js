40408d6374fe18f6f6722cfbd5d446432d111aee
syntaxland <chibuzo.okenwa@email.com>
2024-04-28 16:56:23 +0100

    new update

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -4,6 +4,6 @@
 // export const PAYSOFTER_API_URL = "http://192.168.43.4:8001";
 
 export const API_URL = "https://backend.sellangle.com";
-export const PAYSOFTER_API_URL = "https://api.paysofter.com";  
+export const PAYSOFTER_API_URL = "https://api.paysofter.com"; 
 
 
